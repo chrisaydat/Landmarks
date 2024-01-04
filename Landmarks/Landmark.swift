b2e7bb3c1ce54d96a2c3ede09044fecb80f7af7e
chrisaydat <chrisaydat@gmail.com>
2024-01-04 03:55:42 +0000

    changes

---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,8 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Festus Chris Otopa Ayeh-Datey on 04/01/2024.
+//
+
+import Foundation
---
+++ Landmarks/MapvIew.swift
@@ -0,0 +1,18 @@
+//
+//  MapvIew.swift
+//  Landmarks
+//
+//  Created by Festus Chris Otopa Ayeh-Datey on 04/01/2024.
+//
+
+import SwiftUI
+
+struct MapvIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapvIew()
+}
